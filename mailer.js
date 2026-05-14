8f64430f883f4d5808817bdb114ec10c698bd661
root <root@DESKTOP-B33UFQ8.localdomain>
2026-05-14 00:21:52 -0700

    Updated SMTP production configuration

--- mailer.js
+++ mailer.js
@@ -8,16 +8,57 @@ const transporter = nodemailer.createTransport({
 
   port: Number(process.env.SMTP_PORT),
 
-  secure: false,
+  secure: true,
 
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
   },
 
-  tls: {
-    rejectUnauthorized: false
+  connectionTimeout: 30000,
+
+  greetingTimeout: 30000,
+
+  socketTimeout: 30000
+
+});
+
+transporter.verify((error, success) => {
+
+  if (error) {
+
+    console.log("EMAIL ERROR:", error);
+
+  } else {
+
+    console.log("SMTP SERVER READY");
+
   }
+
+});
+
+module.exports = transporter;require("dotenv").config();
+
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+
+  host: process.env.SMTP_HOST,
+
+  port: Number(process.env.SMTP_PORT),
+
+  secure: true,
+
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  },
+
+  connectionTimeout: 30000,
+
+  greetingTimeout: 30000,
+
+  socketTimeout: 30000
 
 });
 
